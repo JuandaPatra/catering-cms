abccaa6b4daa5e4d215282e451018ad690989669
R. Juanda Patra <patrajuanda10@gmail.com>
2026-04-25 16:09:28 +0700

    fixing:redirect to invoice

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('invoice.index');
+        return redirect()->route('invoice.index');
     }
 }
